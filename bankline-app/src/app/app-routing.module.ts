b9beb3a96f637efc7e0f2c841fa9626894005b59
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-05-09 22:00:56 -0300

    feat: modificando components,

--- bankline-app/src/app/app-routing.module.ts
+++ bankline-app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'movimentacoes-new', component: MovimentacaoNewComponent},
+  {path: 'movimentacoes', component: MovimentacaoListComponent},
+  {path: '', redirectTo: 'movimentacoes', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- bankline-app/src/app/app.component.ts
+++ bankline-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bankline-app';
+  title = 'Bankline APP';
 }
